41ebfae56a599007dd35278c0e9a5fc655ed1e86
Veronicandemo <ndemoveronica@gmail.com>
2023-07-10 03:10:08 +0300

    landing page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: '#001AFF',
+        lightBlue: '#e6e2ff',
+      },
+    },
+  },
+  plugins: [],
+}
